81e918beee3720539c912122eccb78e362dddea8
danis1996 <danisandrios@gmail.com>
2019-09-04 22:29:00 +0300

    add show profil

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   get 'static_pages/index'
   get 'static_pages/secret'
   devise_for :users
+  resources :users, only: [:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
