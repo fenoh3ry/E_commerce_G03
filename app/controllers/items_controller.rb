f6fb7f0b846f163bd0d1302039b4b98b7f3fbec1
danis1996 <danisandrios@gmail.com>
2019-09-06 02:52:54 +0300

    Creating active strorage in development mode, we can now add cat_pic; the seed has been modified and we no longer need the imagr_url attribute for the ITEM

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,9 +10,17 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      flash[:success] = "Description ajouté! maintenant upload votre cat_pic"
+      redirect_to new_item_pic_path(@item.id)
+    else
+      render :new
+    end
   end
 
   def update
@@ -24,4 +32,11 @@ class ItemsController < ApplicationController
   def edit
   end
 
+  private
+    def item_params
+      params.require(:item).permit(
+                  :title, 
+                  :description,
+                  :price)
+    end
 end
