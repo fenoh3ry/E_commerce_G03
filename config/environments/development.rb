149263650f88b3c757b3cb3d957ef9921483e9ec
Francette7 <52659117+Francette7@users.noreply.github.com>
2019-09-05 16:24:46 +0300

    Update development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -7,8 +7,8 @@ Rails.application.configure do
   config.cache_classes = false
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   
- config.action_mailer.delivery_method = :letter_opener
-config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = false
   
 
   # Do not eager load code on boot.
