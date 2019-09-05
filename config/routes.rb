b24ec5e9c82ac8c829447a32b00f78a081e16ff8
danis1996 <danisandrios@gmail.com>
2019-09-05 21:58:41 +0300

    admin dashboard creation that displays the status of all commands

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   resources :orders
   resources :join_table_cart_items
   devise_for :users
-  resources :users, only: [:show]
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
