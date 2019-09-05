c92da92595481afa1e5c4b6a8119ce5d29e3c947
Francette <mkt.curlyaho@gmail.com>
2019-09-05 11:21:25 +0300

    Add order button

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,6 @@
 class StaticPagesController < ApplicationController
   before_action :authenticate_user!, only: [:secret]
   after_create :UserMailer.welcome_email(self).deliver_now
-  def index
-  end
-
-  def secret
-  end
+ 	def index
+  	end
 end
